fd760ec22ccd7f2a99436716ee9adf9cc16552e3
vnq03 <vietnq021103@gmail.com>
2025-02-25 15:53:37 +0700

    Add project files.

---
+++ BookLibraryProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BookLibraryProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
